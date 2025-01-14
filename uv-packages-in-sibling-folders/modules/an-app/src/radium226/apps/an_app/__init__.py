20b4bea3f3ee133ca3ee5237d15a0ad25fdc6e1c
Radium <adrien.besnard@gmail.com>
2025-01-14 12:09:38 +0100

    Try to have a multi-package project with uv (#16)

---
+++ uv-packages-in-sibling-folders/modules/an-app/src/radium226/apps/an_app/__init__.py
@@ -0,0 +1,5 @@
+from radium226.tools.a_tool import Tool
+
+def app():
+    tool = Tool()
+    tool.do_some_stuff()
---
+++ uv-packages-in-sibling-folders/tools/a-tool/src/radium226/tools/a_tool/__init__.py
@@ -0,0 +1,7 @@
+class Tool():
+
+    def __init__(self):
+        pass
+
+    def do_some_stuff(self):
+        print("I'm doing some stuff")
